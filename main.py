809079e827449df7a5ca1c40435a82704291b3f4
Garry <badyalgaurav0@gmail.com>
2023-11-18 10:56:58 +0900

    test

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 from bson.objectid import ObjectId
-from interface.interfaces import router as api_router
+# from interface.interfaces import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from config import settings
+MONGODB_CONN_STR = settings.MONGODB_CONN_STR
 
+# Connection to MongoDB
+client = MongoClient(MONGODB_CONN_STR)
+
+# database
+db = client["UUAABBCC"]
+
+# collections
+users_collection = db["users"]
+accounts_collection = db["accounts"]
+contents_collection = db["contents"]
+draft_collection = db["draftContents"]
+class_collection = db["class"]
+notes_collection = db["notes"]
+submit_assignment_collection = db["submitAssignment"]
 app = FastAPI(debug=True)
 
 @app.get("/")
@@ -14,9 +31,16 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-@app.get("/garry/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+@app.get("/login")
+async def login(email: str, password: str):
+    user = users_collection.find_one({"emailId": email})
+    user["_id"] = str(user["_id"])
+    user["classId"] = str(user["classId"])
+    user_list = [user]
+    if user and bcrypt.verify(password, user["passwordHash"]):
+        return {"data": user, "Message": "success"}
+    raise HTTPException(status_code=401, detail="Invalid username or password")
 
 @app.on_event("startup")
 async def startup():
@@ -35,7 +59,7 @@ app.add_middleware(
     allow_credentials=True
 )
 
-app.include_router(api_router)
+# app.include_router(api_router)
 
 # if __name__ == '__main__':
 #     uvicorn.run("main:app",
